1d7c0a473b745b3df37a5790519bb2d9dab65653
Rumman <mohammad.rumman@accelance.io>
2026-03-04 12:52:15 +0530

    refactor(app): update bootstrap and module registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,34 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { databaseConfig } from './config/database.config';
 import { EmployeeModule } from './modules/employee/employee.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        databaseConfig(configService),
+      useFactory: (c: ConfigService) => databaseConfig(c),
     }),
-
+    AuthModule,
+    UserModule,
     EmployeeModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
